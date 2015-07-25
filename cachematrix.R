d9b21bc2b30b8a65bf0c70e5168f4566fe1e7613
unknown <vikashbh043@gmail.com>
2015-07-25 17:34:26 -0400

    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,29 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    # stores the cached value
+    # initialize to NULL
+    cache <- NULL
+    # create the matrix in the working environment
+    set <- function(y) {
+      x <<- y
+      cache <<- NULL
+    }
+    # get the value of the matrix
+    get <- function() x
+    # invert the matrix and store in cache
+    setMatrix <- function(inverse) cache <<- inverse
+    # get the inverted matrix from cache
+    getInverse <- function() cache
+    
+    # return the created functions to the working environment
+    list(set = set, get = get,
+         setMatrix = setMatrix,
+         getInverse = getInverse)
+  }
 
-}
 
 
 ## Write a short comment describing this function
