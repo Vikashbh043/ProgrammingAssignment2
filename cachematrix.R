8d4b4e48c65099b668bf1b0a7f401bbfbc35f3d6
unknown <vikashbh043@gmail.com>
2015-07-25 17:58:05 -0400

    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,4 @@
+
 ## Put comments here that give an overall description of what your
 ## functions do
 ## Write a short comment describing this function
@@ -29,5 +30,44 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## attempt to get the inverse of the matrix stored in cache
+  cache <- x$getInverse()
+  
+  # return inverted matrix from cache if it exists
+  # else create the matrix in working environment
+  if (!is.null(cache)) {
+    message("getting cached data")
+    
+    # display matrix in console
+    return(cache)
+  }
+  
+  # create matrix since it does not exist
+  matrix <- x$get()
+  
+  # make sure matrix is square and invertible
+  # if not, handle exception cleanly
+  tryCatch( {
+    # set and return inverse of matrix
+    cache <- solve(matrix, ...)
+  },
+  error = function(e) {
+    message("Error:")
+    message(e)
+    
+    return(NA)
+  },
+  warning = function(e) {
+    message("Warning:")
+    message(e)
+    
+    return(NA)
+  },
+  finally = {
+    # set inverted matrix in cache
+    x$setMatrix(cache)
+  } )
+  
+  # display matrix in console
+  return (cache)
 }
